08e1aa19b81dfbd76fef43fc34deecd8a93c940c
lrbrwnn1 <lrbrwnn1@users.noreply.github.com>
2019-05-27 19:54:29 -0500

    Update pubmedScraper.py

--- pubmedScraper.py
+++ pubmedScraper.py
@@ -1,31 +1,45 @@
 from Bio import Entrez
 from Bio import Medline
 
-#TEXT BASED PUBMED SCRAPER
+#list of all keywords that can be used to retrieve info from the medline db
+# Includes 
+#https://biopython-tutorial.readthedocs.io/en/latest/notebooks/09%20-%20Accessing%20NCBIs%20Entrez%20databases.html
+
 def fetch_details(id_list):
     try:
         ids = ','.join(id_list)#Combines id's from id_list
-        Entrez.email = 'xxxx@xxxx.com' #enter your own email address here
-        handle = Entrez.efetch(db="pubmed", id=ids, rettype="medline", retmode="text") 
+        Entrez.email = 'lrbrwnn1@memphis.edu'
+        handle = Entrez.efetch(db="pubmed", id=ids, rettype="medline", retmode="text")
         records = Medline.parse(handle)
+
         return records
-    except: #If the script encounters an error it will ignore it and keep going. This is a workaround for a small number of very old publications that don't seem to play nice with the API
+    except:
         pass
 
-if __name__ == '__main__': #main function
-	#The list of PMID's we want info from. Bio.Entrez has various search methods to get a list of PMIDs if you don't already have the PMID's you need.
-    id_list = ['10028039', '10081590']#sample PMIDs
-    papers = fetch_details(id_list) #calls the detch_details method above, passes id_list as its argument
-    f = open('pubs.txt', 'a+') #creates a text file called pubs.txt in current directory - results are stored here.
-    for paper in papers:
-    	f.write("PMID:"+ paper.get("PMID", "?")+"\n")
-    	f.write("Title:"+ paper.get("TI", "?")+"\n")
-    	f.write("Authors:"+ str(paper.get("AU", "?"))+"\n")
-    	f.write("Affiliation:"+ paper.get("AD", "?")+"\n")
-    	f.write("Abstract:"+ paper.get("AB", "?")+"\n")
-    	f.write("Source:"+ paper.get("SO", "?")+"\n")
-    	f.write("Data Published:"+ paper.get("DP", "?")+"\n")
-    	f.write("Mesh Headings:"+ str(paper.get("MH", "?"))+"\n\n\r")
+#for each author tag (1 per line):
+  #1 - Go through the list of pmid's and get info on each
+  #2 - Append author_tag
+  #3 - Write to file
+if __name__ == '__main__':  
+  count = 0 
+  f = open('pubs.txt', 'a+')
+  with open('author2citation.txt') as a:
+    for line in a:
+      investigator_tag = (line.split('\t')[0])
+      pmidList = (line.split('\t')[1]).rstrip()
+      pmidList = pmidList.split(',')
+      print(investigator_tag, pmidList)
+      papers = fetch_details(pmidList)
+      for paper in papers:
+        count+=1
+        f.write(paper.get("PMID", "?")+"\t")
+        f.write(paper.get("TI", "?")+"\t")
+        f.write(str(paper.get("AU", "?"))+"\t")
+        f.write(paper.get("AB", "?")+"\t")
+        f.write(paper.get("SO", "?")+"\t")
+        f.write(paper.get("DP", "?")+"\t")
+        f.write(str(paper.get("MH", "?"))+"\t")
+        f.write(investigator_tag+"\n")
 
 
 
